57088bf3d4e854c70df3831f152911a21df7b8d7
Richard Silva <rfs20004@gmail.com>
2018-08-18 18:21:05 -0300

    A simple draw app

---
+++ draw.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3.4
+
+from tkinter import *
+
+
+class Drawing(object):
+
+    def display(self):
+
+        root = Tk()
+
+        canvas = Canvas(root, width=500, height=500)
+        canvas.grid(row=2,  columnspan=5)
+
+        colors_label = Label(root, text='R - Red')
+        colors_label.grid(row=0, column=0, padx=2, pady=5, sticky=W+E)
+        colors_label.config(background='red', foreground='white')
+
+        colors_label = Label(root, text='G - Green')
+        colors_label.grid(row=0, column=1, padx=2, pady=5, sticky=W+E)
+        colors_label.config(background='green', foreground='white')
+
+        colors_label = Label(root, text='B - Blue')
+        colors_label.grid(row=0, column=2, padx=2, pady=5, sticky=W+E)
+        colors_label.config(background='blue', foreground='white')
+
+        colors_label = Label(root, text='A - Black')
+        colors_label.grid(row=0, column=3, padx=2, pady=5, sticky=W+E)
+        colors_label.config(background='black', foreground='white')
+
+        colors_label = Label(root, text='C - Delete')
+        colors_label.grid(row=0, column=4, padx=2, pady=5, sticky=W+E)
+        colors_label.config(background='gray30', foreground='white')
+
+        draw_color = 'black'
+
+        def move_mouse(event):
+
+            canvas.create_rectangle(event.x-2, event.y-2, event.x+2,
+                                    event.y+2, fill=draw_color, outline=draw_color)
+
+        canvas.bind('<B1-Motion>', move_mouse)
+
+        def key_press(event):
+            nonlocal draw_color
+            ch = event.char.upper()
+            if ch == 'C':
+                canvas.delete('all')
+            elif ch == 'R':
+                draw_color = 'red'
+            elif ch == 'G':
+                draw_color = 'green'
+            elif ch == 'B':
+                draw_color = 'blue'
+            elif ch == 'A':
+                draw_color = 'black'
+
+        canvas.bind('<KeyPress>', key_press)
+        canvas.focus_set()
+
+        root.mainloop()
+
+
+if __name__ == '__main__':
+    app = Drawing()
+    app.display()
